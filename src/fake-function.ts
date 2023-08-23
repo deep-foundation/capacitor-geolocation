ee5d0eae9468a193315d844249ca94516408ea02
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-23 21:51:08 +0600

    Initial commit

---
+++ src/fake-function.ts
@@ -0,0 +1,10 @@
+import {} from ''
+
+export function makePositionInsertOperations(options: MakePositionInsertOperations) {
+   
+}
+
+export interface MakePositionInsertOperations {
+  deep: DeepClient;
+  position: Position;
+}
---
+++ src/main.ts
@@ -0,0 +1,2 @@
+export {fakeFunction} from './fake-function'
+export {PACKAGE_NAME} from './package-name'
