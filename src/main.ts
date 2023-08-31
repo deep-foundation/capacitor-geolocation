c21bafa7eaeb72ab413982add7222262ca34f75d
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-31 17:20:50 +0600

    Export createGeolocationDecorator

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-
-export {Package} from './package.js'
+export {
+  createGeolocationDecorator,
+  CheckPermissionsResult,
+  GeolocationDecorator,
+  GetPositionOptions,
+  GetPositionResult,
+  InsertPositionOptions,
+  InsertPositionResult,
+  MakePositionInsertOperationsOptions,
+  MakePositionInsertOperationsResult,
+  MakeUpdateUpdatePositionOperationsOptions,
+  MakeUpdateUpdatePositionOperationsResult,
+  RequestPermissionsResult,
+  UpdatePositionOptions,
+  UpdatePositionResult,
+  WatchPositionOptions,
+  WatchPositionResult
+} from './create-geolocation-decorator.js'
+export { Package } from './package.js'
