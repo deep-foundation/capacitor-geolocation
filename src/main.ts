6ede428888dde3c874fa0bf28575965611d88dc9
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-31 17:20:11 +0600

    Remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,2 @@
-export {insertPosition, type InsertPositionOptions} from './insert-position.js'
-export {updatePosition, type UpdatePositionOptions} from './update-position.js'
+
 export {Package} from './package.js'
