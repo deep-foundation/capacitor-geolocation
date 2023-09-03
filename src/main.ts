8472ca1e3e456e34634a86c3e7e333d3f2a4d59a
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-03 19:21:09 +0600

    Export with aliases

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ export { checkPermissions } from './check-permissions.js'
 export { clearWatch } from './clear-watch.js'
 export {
   createGeolocationDecorator,
+  createGeolocationDecorator as createDecorator,
   GeolocationDecorator,
+  GeolocationDecorator as Decorator,
 } from './create-geolocation-decorator.js'
 export { getPosition } from './get-position.js'
 export { insertPosition } from './insert-position.js'
 export { makePositionInsertOperations } from './make-position-insert-operations.js'
-export { makePositionValueUpdateOperations as makePositionUpdateOperations } from './make-position-value-update-operations.js'
+export { makePositionValueUpdateOperations } from './make-position-value-update-operations.js'
 export { Package } from './package.js'
 export { requestPermissions } from './request-permissions.js'
 export { REQUIRED_PACKAGES_IN_MINILINKS } from './required-packages-in-minilnks.js'
