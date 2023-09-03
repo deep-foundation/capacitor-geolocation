7c65dae1565de29f57db7b3e564540b05e317404
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-03 18:05:55 +0600

    Use links instead of value in getPosition

--- src/get-position.ts
+++ src/get-position.ts
@@ -1,6 +1,7 @@
 import { Position } from "@capacitor/geolocation";
 import debug from "debug";
 import { GeolocationDecorator } from "./create-geolocation-decorator.js";
+import { MinilinkCollection, MinilinksResult } from "@deep-foundation/deeplinks/imports/minilinks.js";
 
 export async function getPosition(this: GeolocationDecorator, options: GetPositionOptions): GetPositionResult {
   const log = debug(`@deep-foundation/capacitor-geolocation:${getPosition.name}`);
@@ -10,9 +11,29 @@ export async function getPosition(this: GeolocationDecorator, options: GetPositi
   if (!positionLink) {
     throw new Error(`Failed to find link with id ##${options.linkId}`)
   }
-
-  return positionLink.value?.value;
+  const {data: containTreeLinksDownToPositionLink} = await this.select({
+    up: {
+      tree_id: this.idLocal("@deep-foundation/core", "containTree"),
+      parent_id: positionLink.id,
+    }
+  })
+  const minilinksCollection = new MinilinkCollection();
+  minilinksCollection.apply(containTreeLinksDownToPositionLink);
+  return {
+    coords: {
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","Accuracy"]}})[0]?.value?.value ?? {}),
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","Altitude"]}})[0]?.value?.value ?? {}),
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","AltitudeAccuracy"]}})[0]?.value?.value ?? {}),
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","Heading"]}})[0]?.value?.value ?? {}),
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","Latitude"]}})[0]?.value?.value ?? {}),
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","Longitude"]}})[0]?.value?.value ?? {}),
+      ...(minilinksCollection.query({id: {_id: [positionLink.id, "Coords","Speed"]}})[0]?.value?.value ?? {}),
+    },
+    ...(minilinksCollection.query({id: {_id: [positionLink.id, "Timestamp"]}})[0]?.value?.value ?? {}),
+  };
 }
+
+
 
 export type GetPositionOptions = { linkId: number}
 export type GetPositionResult = Promise<Partial<Position> | undefined>
