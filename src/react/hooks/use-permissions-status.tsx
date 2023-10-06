d3d874468e7db32f64351ebe439c78fa63e2430d
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-06 23:05:40 +0600

    Check permissions status on app resume

--- src/react/hooks/use-permissions-status.tsx
+++ src/react/hooks/use-permissions-status.tsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from "react";
 import { checkPermissions } from "../../check-permissions.js";
 import { PermissionStatus } from "../../permission-status.js";
+import { App } from "@capacitor/app"; // Make sure to import the App from capacitor.
 
 export function usePermissionsStatus(): UsePermissionsStatusResult {
   const [permissionsStatus, setPermissionsStatus] = useState<
     PermissionStatus | undefined
   >(undefined);
-
   const [isLoading, setIsLoading] = useState(true);
 
+  const updatePermissionsStatus = async () => {
+    const permissionsStatus = await checkPermissions();
+    setPermissionsStatus(permissionsStatus);
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    checkPermissions().then((permissionsStatus) => {
-      setPermissionsStatus(permissionsStatus);
-      setIsLoading(false);
-    });
+    updatePermissionsStatus();
+
+    const resumeListener = App.addListener("resume", updatePermissionsStatus);
+
+    return () => {
+      resumeListener.remove();
+    };
   }, []);
 
   return {
